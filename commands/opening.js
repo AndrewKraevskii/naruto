0ced0017dd3ec36c097cfef2350a1cdafcfc394d
Andrew <andrew.kraevskii@gmail.com>
2021-07-20 21:55:30 +0300

    add арка command

--- commands/opening.js
+++ commands/opening.js
@@ -4,7 +4,7 @@ module.exports = {
     aliases: [],
     number: 5,
     execute(client, channel, tags, message) {
-        if (message === '' || message.split(/ +/).length !== 1 || !Math.isInteger(+message)) return;
+        if (message === '' || message.split(/ +/).length !== 1 || !Number.isInteger(+message)) return;
         const openings = ['https://youtu.be/vxvP9zSOL7s', 'https://youtu.be/mFLZ-I2r3Mc', 'https://youtu.be/aJRu5ltxXjc', 'https://youtu.be/kmL3EuiiWXM', 'https://youtu.be/H3aqR_n3sM4',
             'https://youtu.be/SavhHnWla6c', 'https://youtu.be/lGTp4VogNaQ', 'https://youtu.be/L2ncHzfts_0', 'https://youtu.be/KmrTuNXVrf4', 'https://youtu.be/yu12tTrkJ-g',
             'https://youtu.be/ZR5ZCVQ0puE', 'https://youtu.be/PBsMuTPEJ_A', 'https://youtu.be/cjbA7th2rWo', 'https://youtu.be/lBPqob1QYL4', 'https://youtu.be/_I1N2JFDBwU',
