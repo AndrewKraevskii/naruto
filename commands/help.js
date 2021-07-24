ed335523e9cb923a570724f15101cc31fadb33ab
Andrew <andrew.kraevskii@gmail.com>
2021-07-24 19:08:56 +0300

    .

--- commands/help.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'help',
-    cooldown: 5,
-    execute(client, channel, tags, message) {
-        client.say(channel, `@${tags.username} https://bit.ly/2W7ECQl`);
-    },
-};
