88b210a3e570e9214312b60bfdce7bf925315f10
Andrew <andrew.kraevskii@gmail.com>
2021-07-25 00:16:24 +0300

    normal description loading

--- commands/серия.js
+++ commands/серия.js
@@ -1,39 +1,11 @@
 var fs = require('fs');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const _episods = [];
-fs.readFile('./res/наруто описание.html', 'utf8', (err, data) => {
-    dom = new JSDOM(data);
-    list = dom.window.document.querySelectorAll('#k2Container > div.itemBody > div.itemFullText > p');
-
-    for (let i = 0; i < list.length; ++i) {
-        let text = list[i].textContent;
-        if (/Сезон.*/.exec(text)) {
-            continue;
-        }
-
-        const res = /(\d+) серия\((.*)\)\(/.exec(text);
-        if (res) {
-            _episods.push({
-                "number": res[1] - 220,
-                "name": res[2],
-                "description": ""
-            });
-
-            if (i + 1 < list.length && (/(\d+) серия\((.*)\)\(/.exec(list[i + 1].textContent)) === null && (/Сезон.*/.exec(list[i + 1].textContent)) === null) {
-                _episods[_episods.length - 1].description = list[i + 1].textContent;
-            }
-        }
-
-    }
-});
 
 
-module.exports = {
+const obj = {
     name: 'арка',
     cooldown: 1,
     aliases: [],
-    episods: _episods,
+    episods: JSON.parse(fs.readFileSync('./res/naruto.json', 'utf8')),
     execute(client, channel, tags, message) {
         if (message === '' || message.split(/ +/).length !== 1 || !Number.isInteger(+message)) return;
 
@@ -47,3 +19,9 @@ module.exports = {
 
     },
 };
+
+
+
+
+
+module.exports = obj;
