0216d536c6efd9b8df65bd1acf70a9d994ef6973
Andrew <andrew.kraevskii@gmail.com>
2021-09-09 23:09:27 +0300

    add egs command

---
+++ commands/egs.js
@@ -0,0 +1,21 @@
+const { getGames } = require("epic-free-games");
+
+function getFreeGames(gamesArray) {
+    return gamesArray.currents.filter(item => item.price.totalPrice.discountPrice === 0);
+}
+
+module.exports = {
+    name: 'нарутобот',
+    cooldown: 5,
+    async execute(client, channel, tags, message) {
+        try {
+            const games = getFreeGames(await getGames("RU"));
+            const game_names = games.map(item => item.title);
+            client.say(channel, `В egs раздают: ${game_names.join(', ')}`);
+        }
+        catch (error) {
+            console.log(error)
+            client.say(channel, `Произошла какая то ошибка, обратитесь к @andrewkraevskii за помощью`)
+        }
+    },
+};
